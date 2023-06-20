6b9ae61378cf1e7c5cb402f12ddc7b8a551647f2
manato <c0b2201096@edu.teu.ac.jp>
2023-06-20 14:49:06 +0900

    練習２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -18,20 +18,22 @@ def main():
     y = random.randint(0,HEIGHT)
     bd_rct = bd_img.get_rect()
     bd_rct.center = x,y
+    vx, vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-
+ 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        bd_rct.move_ip(vx, vy)
         screen.blit(bd_img,bd_rct)
         pg.display.update()
         tmr += 1
         clock.tick(10)
-
+        clock.tick(50)
 
 if __name__ == "__main__":
     pg.init()
